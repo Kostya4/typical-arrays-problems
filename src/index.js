b546adbf3d4012ea2af97930dcbb5713740b8aa9
Kostya4 <=>
2020-09-29 19:30:44 +0300

    feat: add fun min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+  
+  let minValue = Number.MAX_VALUE
+  array.forEach(element => {
+    if (minValue > element) {
+      minValue = element
+    }
+  });    
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+if (typeof array === 'undefined' || array.length == 0) return 0;
+
+let maxValue = Number.MIN_VALUE
+array.forEach(element => {
+  if (maxValue < element) {
+    maxValue = element
+  }
+  });
+  return maxValue
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+
+  let sum = 0
+  array.forEach(element => {
+    sum += element  
+  });  
+  return sum / array.length 
 }
